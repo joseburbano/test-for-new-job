fa12b1ee8fc9324ee8747610f848898a2d702ec3
jose manuel burbano <jose.burbano@getpulpo.com>
2024-03-05 20:26:26 -0500

    test

--- src/main/java/com/example/testfornewjob/TestForNewJobApplication.java
+++ src/main/java/com/example/testfornewjob/TestForNewJobApplication.java
@@ -1,5 +1,4 @@
 package com.example.testfornewjob;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,5 +8,6 @@ public class TestForNewJobApplication {
     public static void main(String[] args) {
         SpringApplication.run(TestForNewJobApplication.class, args);
     }
+
 
 }
