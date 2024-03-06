5383fe534584e718fb5176f1ca15cd20212af39a
jose manuel burbano <jose.burbano@getpulpo.com>
2024-03-05 20:06:47 -0500

    test

--- src/test/java/com/example/testfornewjob/configurations/challenge/domain/services/ChallengeServiceImplTest.java
+++ src/test/java/com/example/testfornewjob/configurations/challenge/domain/services/ChallengeServiceImplTest.java
@@ -1,12 +1,15 @@
 package com.example.testfornewjob.configurations.challenge.domain.services;
 
-import static org.junit.jupiter.api.Assertions.*;
 import com.example.testfornewjob.configurations.challenge.domain.model.ChallengeDTO;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(MockitoExtension.class)
 class ChallengeServiceImplTest {
@@ -19,14 +22,14 @@ class ChallengeServiceImplTest {
     void testCalculateMaximum() {
         // Test case 1: x=7, y=5, n=12345
         ChallengeDTO testCase1 = new ChallengeDTO(7, 5, 12345);
-      //  assertEquals(12339, challengeServiceImpl.calculateMaximum(testCase1));
+        assertEquals(12339, Optional.ofNullable(challengeServiceImpl.calculateMaximum(testCase1)));
 
         // Test case  2: x=5, y=0, n=4
         ChallengeDTO testCase2 = new ChallengeDTO(5, 0, 4);
-       // assertEquals(0, challengeServiceImpl.calculateMaximum(testCase2));
+        assertEquals(0, Optional.ofNullable(challengeServiceImpl.calculateMaximum(testCase2)));
 
         // Test case  3: x=10, y=5, n=15
         ChallengeDTO testCase3 = new ChallengeDTO(10, 5, 15);
-       // assertEquals(15, challengeServiceImpl.calculateMaximum(testCase3));
+        assertEquals(15, Optional.ofNullable(challengeServiceImpl.calculateMaximum(testCase3)));
     }
 }
